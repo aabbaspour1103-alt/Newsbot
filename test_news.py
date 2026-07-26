65bdb2c6c1c52a83387bdf9a9575a7129c72f9f5
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-26 16:13:31 +0330

    Update test_news.py

--- test_news.py
+++ test_news.py
@@ -1,25 +1,28 @@
 from news_scraper import get_news
 from translator import translate_text
+from news_filter import is_new_news
 
 
 news = get_news()
 
-
-print("تعداد خبرها:", len(news))
+print("تعداد خبرهای دریافت شده:", len(news))
 
 
 for item in news:
 
-    translated_title = translate_text(item["title"])
+    if is_new_news(item["link"]):
 
-    print("\n📰 منبع:", item["source"])
-    print("🇬🇧 عنوان اصلی:")
-    print(item["title"])
+        translated_title = translate_text(item["title"])
 
-    print("🇮🇷 ترجمه:")
-    print(translated_title)
+        print("\n🆕 خبر جدید")
+        print("📰 منبع:", item["source"])
+        print("🇮🇷 ترجمه:")
+        print(translated_title)
+        print("🔗 لینک:")
+        print(item["link"])
 
-    print("🔗 لینک:")
-    print(item["link"])
+        print("-" * 60)
 
-    print("-" * 60)
+    else:
+        print("⏭ خبر تکراری حذف شد:")
+        print(item["title"])
